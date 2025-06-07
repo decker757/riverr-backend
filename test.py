0477b797a19bc69433ba291e29aceaa4ed6104cd
Ernest Tan <134849903+decker757@users.noreply.github.com>
2025-06-08 01:12:37 +0800

    Update test.py

--- test.py
+++ test.py
@@ -28,14 +28,22 @@ from xrpl.transaction import autofill_and_sign, submit_and_wait
 # response = submit_and_wait(signed, client)
 # print(response.result)
 
-TrustLine.send_issued_currency(
+TrustLine.create_trustline(
     wallet=wallet1,
-    destination=wallet2.classic_address,
-    currency_code="ETH",
-    issuer=wallet3.classic_address,
-    amount=100.0,
+    issuer_address=wallet3.classic_address,
+    currency_code="RLUSD",
+    limit=10000000,
     client=client
 )
+
+# TrustLine.send_issued_currency(
+#     wallet=wallet1,
+#     destination=wallet2.classic_address,
+#     currency_code="ETH",
+#     issuer=wallet3.classic_address,
+#     amount=100.0,
+#     client=client
+# )
 # TrustLine.clear_no_ripple_flag(
 #     wallet=wallet3,  # issuer
 #     issuer_address=wallet1.classic_address,
@@ -54,3 +62,4 @@ TrustLine.send_issued_currency(
 
 # lines = AccountLines(account=wallet1.classic_address, peer=wallet3.classic_address)
 # print(client.request(lines).result)
+
